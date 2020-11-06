6f0c5e4b303f31d517f5c0e222a48487c5d26823
YujingGao <yujinggao0820@gmail.com>
2020-11-06 16:52:36 -0500

    Fix NWheat problem

--- EvaluateProcessing.r
+++ EvaluateProcessing.r
@@ -6,63 +6,35 @@ eval(parse(text=paste("File<-readLines('",OD,"/Evaluate_output.txt',n=-1)",sep =
 FileLength<-length(File);
 #print(FileLength);
 
-if ((CropName != "BA") & (CropName != "WH") & (CropName != "CS"))   
-{
-TreatmentNumber<-(FileLength-3);##Get the number of treatments.
-#print(TreatmentNumber);
+TitleLine = grep("@RUN",File)
 
-FrameTitle<-substr(File[3],2,500);
+FrameTitle<-File[TitleLine]
 FrameData<-c();
 ##Read the titles of the predictions, which is located in the 3rd line of the evaluate output.
 #FrameData<-c(); ##Creat an empty frame to store the selected data.
 
-SummaryFrame<-data.frame()
-
-for (j in 1:TreatmentNumber)
-{
-n=3+j;##Set the starting line for data reading in an individual model run, staring from the 4th line.
-FrameData=rbind(FrameData,File[n]);##Combine the results in one matrix.
-}
-
-if(ModelRunNumber!=1)
-{
-EvaluateFrame<-FrameData;
-eval(parse(text = paste('write(EvaluateFrame, "',OD,'/EvaluateFrame_',
-RoundOfGLUE,'.txt", append = T)',sep="")));
-#Write the data as a frame file, where repair the treatment names as an entity (" ").
-} else
-{
-EvaluateFrame<-rbind(FrameTitle,FrameData);
-eval(parse(text = paste('write(EvaluateFrame, "',OD,'/EvaluateFrame_',
-RoundOfGLUE,'.txt", append = F)',sep="")));
-}
-
-} else if (CropName == "BA" || CropName == "WH" || CropName == "CS") ##The eveluate.out file of BA (barley), WH (wheat) is different from the others, such as Maize.
-{
-TreatmentNumber<-(FileLength-5);
-
-FrameTitle<-substr(File[5],2,500);
-FrameData<-c();
+TreatmentNumber<-(FileLength-TitleLine);##Get the number of treatments.
+#print(TreatmentNumber);
 
 SummaryFrame<-data.frame()
 
 for (j in 1:TreatmentNumber)
 {
-n=5+j;##Set the starting line for data reading in an individual model run, staring from the 6th line for Barley.
-FrameData=rbind(FrameData,File[n]);
+  n=TitleLine+j;
+  FrameData=rbind(FrameData,File[n]);##Combine the results in one matrix.
 }
 
 if(ModelRunNumber!=1)
 {
-EvaluateFrame<-FrameData;
-eval(parse(text = paste('write(EvaluateFrame, "',OD,'/EvaluateFrame_',
-RoundOfGLUE,'.txt", append = T)',sep="")));
+  EvaluateFrame<-FrameData;
+  eval(parse(text = paste('write(EvaluateFrame, "',OD,'/EvaluateFrame_',
+                          RoundOfGLUE,'.txt", append = T)',sep="")));
+  #Write the data as a frame file, where repair the treatment names as an entity (" ").
 } else
 {
-EvaluateFrame<-rbind(FrameTitle,FrameData);
-eval(parse(text = paste('write(EvaluateFrame, "',OD,'/EvaluateFrame_',
-RoundOfGLUE,'.txt", append = F)',sep="")));
-}
+  EvaluateFrame<-rbind(FrameTitle,FrameData);
+  eval(parse(text = paste('write(EvaluateFrame, "',OD,'/EvaluateFrame_',
+                          RoundOfGLUE,'.txt", append = F)',sep="")));
 }
 
 rm(File);
